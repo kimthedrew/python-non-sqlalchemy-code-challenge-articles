b1edbb421084122f753f6ab95db89de06f918eb9
kimthedrew <kimutai848@gmail.com>
2024-12-09 01:00:29 +0300

    sqlite

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,118 @@
 class Article:
+    _all = []
+
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+        if not isinstance(author, Author):
+            raise Exception("Author must be an instance of Author class")
+        if not isinstance(magazine, Magazine):
+            raise Exception("Magazine must be an instance of Magazine class")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise Exception("Title must be a string between 5 and 50 characters")
+        
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        Article._all.append(self)
+
+    @property
+    def author(self):
+        return self._author
+    
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @classmethod
+    def all(cls):
+        return cls._all
+
+
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise Exception("Name must be a non-empty string")
+        self._name = name
+
+    @property 
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (2 <= len(value) <= 16):
+            raise Exception("Name must be a non-empty string")
+        self._name = value
 
     def articles(self):
-        pass
+        return [article for article in Article.all() if article.author == self]
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self.articles()})
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        return list({magazine.category for magazine in self.magazines()})
 
 class Magazine:
-    def __init__(self, name, category):
-        self.name = name
-        self.category = category
+    _all = []
 
+    def __init__(self, name, category):
+        if not isinstance(name, str) or not (2 <= len(name) <= 16):
+            raise Exception("Name must be a string between 2 and 16 characters")
+        if not isinstance(category, str) or len(category) == 0:
+            raise Exception("Category must be a non-empty string")
+        
+        self._name = name
+        self._category = category
+        Magazine._all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (2 <= len(value) <= 16):
+            raise Exception("Name must be a string between 2 and 16 characters")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise Exception("Category must be a non-empty string")
+        self._category = value
+    
     def articles(self):
-        pass
+        return [article for article in Article.all() if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self.articles()))
 
     def article_titles(self):
-        pass
+        return [article.title for article in self.articles()]
 
     def contributing_authors(self):
-        pass
+        author_count = {}
+        for article in self.articles():
+            author_count[article.author] = author_count.get(article.author, 0) + 1
+        return [author for author, count in author_count.items() if count > 2]
+    
+    @classmethod
+    def top_publisher(cls):
+        magazine_articles = {magazine: len(magazine.articles()) for magazine in cls._all}
+        top_magazine = max(magazine_articles, key=magazine_articles.get, default=None)
+        return top_magazine.name if top_magazine else None
+    
+
+    
